acc70151ebed85674cd22cfdf903ef208c2fb1da
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-01-15 00:42:37 +0100

    chore(wip): oauth2 google

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,31 @@
+
+const OAUTH2_PATHS = new Set(["/oauth2/google/callback"]);
+
+
 export function onRequest (context:any, next:any) {
+
+    // if (OAUTH2_PATHS.has(context.url.pathname)) {
+    //     console.log('Oauth2 path Google');
+    //     console.log(context)
+    //     // console.log(context.request.headers.get('cookie'))
+
+    // }
+
+
+
     // intercept data from a request
     // optionally, modify the properties in `locals`
     context.locals.title = "New title";
 
-    console.log('Request intercepted');
+    // console.log('Request intercepted');
+    // console.log(context.request.headers.get('cookie'))
+    // console.log(context.url.pathname)
+    // console.log(context.cookies)
+   
+    // if (OAUTH2_PATHS.has(context.url.pathname)) {
+    //     console.log('Oauth2 path');
+    // }
+
 
     // return a Response or the result of calling `next()`
     return next();
